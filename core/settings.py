ee1dea22b59007826539848b11e3f4595ad6b726
Wanderson <wheredia2@gmail.com>
2026-09-13 11:03:13 -0300

    fix: adiciona gunicorn, whitenoise, Procfile e ajusta settings para o Render

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -101,6 +102,7 @@ USE_GCP_STORAGE = config("USE_GCP_STORAGE", default=not DEBUG, cast=bool)
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Mídia Padrão (Local)
 MEDIA_URL = "media/"
