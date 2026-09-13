b04ae4f7050fb9161e446166beae697793184c34
Wanderson <wheredia2@gmail.com>
2026-09-13 11:46:20 -0300

    fix: adiciona .onrender.com ao ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config(
 )
 DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS", default="127.0.0.1,localhost,.web.app,.firebaseapp.com", cast=Csv()
+    "'127.0.0.1', 'localhost', '.web.app', '.firebaseapp.com', '.onrender.com'",
 )
 
 # -----------------------------------------------------------------------------
